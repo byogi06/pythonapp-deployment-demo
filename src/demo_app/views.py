5af73f2eaac3d7da79a32d68cbd7d3b51eb6c7a9
Yogesh Bhagat <45122979+byogi06@users.noreply.github.com>
2024-08-22 13:31:57 +0200

    Delete src directory

--- src/demo_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-
-# Create your views here.
-def home(request,*args,**kwargs):
-    return JsonResponse({"message":"Hi Allianz, I am Yogesh Bhagat"})
